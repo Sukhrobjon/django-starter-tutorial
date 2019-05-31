968ad3f1e00de4104c24f2086efda09d0276a47d
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-05-30 17:04:30 -0700

    [created] admin user

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom Configs
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
