8e9038d2c92eb13cafd6b0506770c86e008bb505
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-14 18:58:53 -0700

    [wrote] test for detail view

--- polls/tests.py
+++ polls/tests.py
@@ -107,3 +107,50 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+            The detail view of a question with a pub_date in the future 
+            returns a 404 not found
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+            The detail view of a question with a pub_date in the past 
+            displays the question's text
+        """
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response.status_code, past_question.question_text)
+
+
+# TODO
+"""
+We ought to add a similar get_queryset method to ResultsView and create 
+a new test class for that view. It’ll be very similar to what
+ we have just created; in fact there will be a 
+lot of repetition.
+
+We could also improve our application in other ways, adding tests 
+along the way. For example, it’s silly that Questions can be 
+published on the site that have no Choices. So, our views could 
+check for this, and exclude such Questions. Our tests would create a 
+Question without Choices and then test that it’s not published, as 
+well as create a similar Question with Choices, and test that it is published.
+
+Perhaps logged-in admin users should be allowed to see unpublished Questions, 
+but not ordinary visitors. Again: whatever needs to be added to the software to 
+accomplish this should be accompanied by a test, whether you write the test 
+first and then make the code pass the test, or work out the logic in your 
+code first and then write a test to prove it.
+
+At a certain point you are bound to look at your tests and wonder 
+whether your code is suffering from test bloat, which brings us to:
+"""
